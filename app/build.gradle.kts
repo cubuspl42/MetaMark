cubuspl42/AntlrKotlinHelloWorld

Integrate ANTLRv4

app/build.gradle.kts
@@ -11,6 +11,8 @@ plugins {
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
+    antlr
 }
 
 repositories {
@@ -29,13 +31,12 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:32.1.1-jre")
+
+    antlr("org.antlr:antlr4:4.13.0")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+kotlin {
+    jvmToolchain(17)
 }
 
 application {
@@ -43,7 +44,22 @@ application {
     mainClass.set("antlrkotlinhelloworld.AppKt")
 }
 
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
+tasks {
+    generateGrammarSource {
+        maxHeapSize = "64m"
+        arguments = arguments + listOf("-package", "antlrkotlinhelloworld.parser.antlr", "-visitor", "-no-listener")
+        outputDirectory = File("${project.buildDir}/generated-src/antlr/main/antlrkotlinhelloworld/parser/antlr")
+    }
+
+    compileKotlin {
+        dependsOn(generateGrammarSource)
+    }
+
+    compileTestKotlin {
+        dependsOn(generateTestGrammarSource)
+    }
+
+    test {
+        useJUnitPlatform()
+    }
 }

app/src/main/kotlin/antlrkotlinhelloworld/parser/RootTerm.kt
@@ -1,0 +1,31 @@
+package antlrkotlinhelloworld.parser
+
+import antlrkotlinhelloworld.parser.antlr.HelloLexer
+import antlrkotlinhelloworld.parser.antlr.HelloParser
+import antlrkotlinhelloworld.parser.antlr.HelloParser.RootContext
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+
+data class RootTerm(
+    val target: String
+) {
+    companion object {
+        fun parse(
+            source: String,
+        ): RootTerm {
+            val sourceName = "__root__"
+
+            val lexer = HelloLexer(CharStreams.fromString(source, sourceName))
+            val tokenStream = CommonTokenStream(lexer)
+            val parser = HelloParser(tokenStream)
+
+            return build(parser.root())
+        }
+
+        fun build(ctx: RootContext): RootTerm {
+            return RootTerm(
+                target = ctx.target.text,
+            )
+        }
+    }
+}

cubuspl42/MetaMark

app/build.gradle.kts
@@ -11,6 +11,8 @@ plugins {
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
+    antlr
 }
 
 repositories {
@@ -29,13 +31,12 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:32.1.1-jre")
+
+    antlr("org.antlr:antlr4:4.13.0")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+kotlin {
+    jvmToolchain(17)
 }
 
 kotlin {
@@ -47,7 +48,22 @@ application {
     mainClass.set("metamark.AppKt")
 }
 
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
+tasks {
+    generateGrammarSource {
+        maxHeapSize = "64m"
+        arguments = arguments + listOf("-package", "metamark.parser.antlr", "-visitor", "-no-listener")
+        outputDirectory = File("${project.buildDir}/generated-src/antlr/main/metamark/parser/antlr")
+    }
+
+    compileKotlin {
+        dependsOn(generateGrammarSource)
+    }
+
+    compileTestKotlin {
+        dependsOn(generateTestGrammarSource)
+    }
+
+    test {
+        useJUnitPlatform()
+    }
 }

app/src/main/kotlin/metamark/parser/RootTerm.kt
@@ -1,0 +1,31 @@
+package metamark.parser
+
+import metamark.parser.antlr.HelloLexer
+import metamark.parser.antlr.HelloParser
+import metamark.parser.antlr.HelloParser.RootContext
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+
+data class RootTerm(
+    val target: String
+) {
+    companion object {
+        fun parse(
+            source: String,
+        ): RootTerm {
+            val sourceName = "__root__"
+
+            val lexer = HelloLexer(CharStreams.fromString(source, sourceName))
+            val tokenStream = CommonTokenStream(lexer)
+            val parser = HelloParser(tokenStream)
+
+            return build(parser.root())
+        }
+
+        fun build(ctx: RootContext): RootTerm {
+            return RootTerm(
+                target = ctx.target.text,
+            )
+        }
+    }
+}