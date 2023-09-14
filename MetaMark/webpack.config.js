b570b9f5a32076102e0e205b7aec6353cde96e26
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-09-14 18:38:00 +0200

    Integrate Webpack

---
+++ MetaMark/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const ShebangPlugin = require("webpack-shebang-plugin");
+module.exports = {
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+  plugins: [
+    new ShebangPlugin(),
+  ],
+};
