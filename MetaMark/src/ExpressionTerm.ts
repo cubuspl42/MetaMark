643a19a8d590151899e982effdc85809fc13d0b5
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-09-16 11:09:08 +0200

    Re-organize and rename a some things

--- MetaMark/src/ExpressionTerm.ts
+++ MetaMark/src/ExpressionTerm.ts
@@ -1,8 +1,9 @@
 import * as typescript_ast from "./typescript_ast";
 
 export abstract class ExpressionRepresentation {
-    abstract buildParseFunctionExpression(): typescript_ast.ExpressionTerm;
+    abstract generateParseFunctionExpression(): typescript_ast.ExpressionTerm;
 }
+
 export abstract class ExpressionTerm {
     abstract get representation(): ExpressionRepresentation;
 }
--- MetaMark/src/typescript_ast/BlockTerm.ts
+++ MetaMark/src/typescript_ast/BlockTerm.ts
@@ -3,18 +3,6 @@ import { ReturnStatementTerm } from "./ReturnStatementTerm";
 import { ExpressionTerm } from "./ExpressionTerm";
 
 export class BlockTerm {
-    static buildReturnBlock(args: {
-        readonly returnedExpression: ExpressionTerm;
-    }) {
-        return new BlockTerm({
-            innerStatements: [
-                new ReturnStatementTerm({
-                    returnedExpression: args.returnedExpression,
-                }),
-            ],
-        });
-    }
-
     readonly innerStatements: ReadonlyArray<StatementTerm>;
 
     constructor(args: {
