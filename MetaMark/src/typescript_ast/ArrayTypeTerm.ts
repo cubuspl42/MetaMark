9d61a1331132d7eb33a2016ba536b9bbb3794aeb
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-09-16 12:21:19 +0200

    Nuke some AST types

--- MetaMark/src/typescript_ast/ArrayTypeTerm.ts
+++
@@ -1,9 +0,0 @@
-import { TypeExpressionTerm } from "./TypeExpressionTerm";
-
-export class ArrayTypeTerm {
-    readonly elementType: TypeExpressionTerm;
-
-    constructor(args: { elementType: TypeExpressionTerm }) {
-        this.elementType = args.elementType;
-    }
-}
