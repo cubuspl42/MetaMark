6b5f69c842f056daaeacc8a7cd90a7f0d455a54b
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-09-18 17:44:01 +0200

    Format the code

--- MetaMark/src/metamark_ast/ConcatExpressionTerm.ts
+++ MetaMark/src/metamark_ast/ConcatExpressionTerm.ts
@@ -6,16 +6,17 @@ import { arrayEquals } from "../utils";
 import * as typescript_ast from "../typescript_ast";
 
 export class ConcatExpressionTerm extends ExpressionTerm {
-    static equals(
-        a: ConcatExpressionTerm,
-        b: ConcatExpressionTerm,
-    ): boolean;
+    static equals(a: ConcatExpressionTerm, b: ConcatExpressionTerm): boolean;
     static equals(a: unknown, b: unknown): boolean | undefined;
 
     static equals(a: unknown, b: unknown): boolean | undefined {
         if (!(a instanceof ConcatExpressionTerm)) return undefined;
         if (!(b instanceof ConcatExpressionTerm)) return undefined;
-        return arrayEquals(a.joinedExpressions, b.joinedExpressions, ExpressionTermUtils.equals);
+        return arrayEquals(
+            a.joinedExpressions,
+            b.joinedExpressions,
+            ExpressionTermUtils.equals,
+        );
     }
 
     static build(
@@ -23,7 +24,9 @@ export class ConcatExpressionTerm extends ExpressionTerm {
         ctx: Expression_concatExpressionContext,
     ): ConcatExpressionTerm {
         return new ConcatExpressionTerm(
-            ctx.expression().map((it) => ExpressionTermUtils.build(staticScope, it)),
+            ctx
+                .expression()
+                .map((it) => ExpressionTermUtils.build(staticScope, it)),
         );
     }
 
