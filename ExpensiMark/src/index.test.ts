2eaf8a6268b863d6ff8c0a67524726acfe8ec23b
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-09-18 17:52:33 +0200

    `ExpensiMark`: Add a test
    ...and fix the runtime a bit

---
+++ ExpensiMark/src/index.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "@jest/globals";
+import { emphasisParser } from "../generated_src/MarkParser";
+import { StringCharStream } from "../generated_src/runtime";
+
+describe("GrammarTerm", () => {
+    test("It parses", () => {
+        const charStream = new StringCharStream("*foo*");
+        const result = emphasisParser.parse(charStream);
+
+        expect(result).not.toBeNull();
+
+        const content = result!.buildNode().content;
+
+        expect(content).toEqual("foo");
+    });
+});
